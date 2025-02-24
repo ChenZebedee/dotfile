1571a0522fc43b27beb2dabcbba1f079c9cd2fe4
shaoti.chen <shaoti.chen@outlook.com>
2025-02-24 20:32:55 +0800

    ts

--- nvim/.config/nvim/lua/plugins/typescript.lua
+++ nvim/.config/nvim/lua/plugins/typescript.lua
@@ -1,10 +1,19 @@
 return {
     {
         "pmizio/typescript-tools.nvim",
-        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-        ft = { "typescript", "typescriptreact" }, -- 仅针对 TypeScript 文件加载
+        dependencies = { "nvim-lua/plenary.nvim" },
+        ft = { "typescript", "typescriptreact", "javascript", "javascriptreact" },
         opts = {
-            -- 自定义配置（可选）
+            settings = {
+                expose_as_code_action = "all",
+                tsserver_file_preferences = {
+                    includeInlayParameterNameHints = "all",
+                    includeCompletionsForModuleExports = true,
+                },
+            },
+            on_attach = function(client, bufnr)
+                -- 自定义 on_attach 逻辑（如绑定 LSP 快捷键）
+            end,
         },
     },
 }
