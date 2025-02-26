b2272b7feb9b7efeaff3cd9a3626a02fc97692d6
shaoti <shaoti.chen@outlook.com>
2025-02-26 17:58:59 +0800

    删除一些mason

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -7,7 +7,6 @@ return {
             "go-debug-adapter",
             "js-debug-adapter",
             "markdownlint-cli2",
-            "phpcs",
             "pyright",
             "stylua",
             "vtsls",
@@ -16,7 +15,10 @@ return {
             "gofumpt",
             "json-lsp",
             "markdown-toc",
-            "php-cs-fixer",
+            -- "phpcs",
+            -- "php-cs-fixer",
+            -- "php-debug-adapter",
+            -- "phpactor",
             "python-lsp-server",
             "tailwindcss-language-server",
             "vue-language-server",
@@ -25,18 +27,38 @@ return {
             "goimports",
             "lua-language-server",
             "marksman",
-            "php-debug-adapter",
             "ruff",
             "taplo",
             "codelldb",
             "eslint-lsp",
             "gopls",
             "markdownlint",
-            "phpactor",
             "pylint",
             "shfmt",
             "typescript-language-server",
             -- 添加你在 :Mason 中看到的其他工具
         },
     },
+    config = function(_, opts)
+        require("mason").setup(opts)
+
+        -- 获取已安装的包
+        local registry = require("mason-registry")
+        local installed_packages = registry.get_installed_package_names()
+
+        -- 定义需要保留的包
+        local desired_packages = opts.ensure_installed or {}
+
+        -- 删除不在 ensure_installed 中的包
+        for _, pkg_name in ipairs(installed_packages) do
+            if not vim.tbl_contains(desired_packages, pkg_name) then
+                local pkg = registry.get_package(pkg_name)
+                print("Uninstalling " .. pkg_name)
+                pkg:uninstall()
+            end
+        end
+
+        -- 安装 ensure_installed 中的包
+        require("mason-lspconfig").setup({ ensure_installed = desired_packages })
+    end,
 }
