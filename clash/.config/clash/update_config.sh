13b3be0794fdb8a4c236dcadc63026affbebcd88
shaoti <shaoti.chen@outlook.com>
2025-03-04 10:42:31 +0800

    更新，clash更新脚本

--- clash/.config/clash/update_config.sh
+++ clash/.config/clash/update_config.sh
@@ -1,1 +1,3 @@
 wget -O $HOME/.config/clash/config.yaml https://sub.ssr.sh/link/nwDK7kG1KaOjl7qQ?clash=2
+
+sed -i 's#allow-lan: false#allow-lan: true\nsecert: 123456\nexternal-ui: /root/clash/dashboard#g' ${HOME}/.config/clash/config.yaml
