caf1e8b080e7af43df0eaa5ae7db2d2bc72f5718
shaoti.chen <shaoti.chen@outlook.com>
2025-03-05 20:05:52 +0800

    新路径

--- clash/.config/clash/update_config.sh
+++ clash/.config/clash/update_config.sh
@@ -1,3 +1,3 @@
-wget -O $HOME/.config/clash/config.yaml https://sub.ssr.sh/link/nwDK7kG1KaOjl7qQ?clash=2
+wget -O $HOME/.config/clash/config.yaml https://feed.iggv5.com/c/b27e0d53-7e93-4d43-bc5e-88b8bcc2f9fc
 
 sed -i 's#allow-lan: false#allow-lan: true\nsecert: 123456\nexternal-ui: /root/clash/dashboard#g' ${HOME}/.config/clash/config.yaml
